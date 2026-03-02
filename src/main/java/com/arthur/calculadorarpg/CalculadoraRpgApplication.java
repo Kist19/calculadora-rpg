0383e41676d192eb38081aa44d8f7d51870d01eb
Kist19 <arthur.kist09@gmail.com>
2026-03-02 18:01:18 -0300

    primeiro commit

---
+++ src/main/java/com/arthur/calculadorarpg/CalculadoraRpgApplication.java
@@ -0,0 +1,13 @@
+package com.arthur.calculadorarpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraRpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraRpgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arthur/calculadorarpg/CalculadoraRpgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthur.calculadorarpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraRpgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
